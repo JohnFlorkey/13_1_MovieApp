92867d85b9b2e76db92a630fdcfbe816dc668bac
John Florkey <john.florkey@gmail.com>
2021-02-07 16:40:13 -0500

    added form validation

--- app.js
+++ app.js
@@ -8,8 +8,29 @@ $('#movies').on('click', '.remove', function(){
     $(this).parent().remove();
 })
 
+function validateRating(rating) {
+    if(rating >= 0 && rating <= 10) {
+        return true;
+    }
+    alert('Movie rating must be a number between 0 and 10');
+    return false;
+}
+
+function validateTitle(title) {
+    if(title.length > 1) {
+        return true;
+    }
+    alert('Movie title must be at least two characters');
+    return false;
+}
+
 function addMovie(title, rating) {
-    // $(`<div> ${title} rates a ${rating} <button>Remove</button></div>`).appendTo($('#movies'));
+    if(!validateRating(rating)) {
+        return;
+    };
+    if(!validateTitle(title)) {
+        return;
+    };
     $('<div>')
         .text(`${title} rates a ${rating} `)
         .append($('<button>').text('Remove').addClass('remove'))
