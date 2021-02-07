47a1a62257c036f58e3d762a8d746b3a28f84ae8
John Florkey <john.florkey@gmail.com>
2021-02-07 17:50:42 -0500

    refactored using Movie class

--- app.js
+++ app.js
@@ -1,39 +1,62 @@
+let movies = [];
+let id = 0;
+
+class Movie {
+    constructor (title, rating) {
+        this.title = title;
+        this.rating = rating;
+        this.id = null;
+    }
+    validateRating() {
+        if(this.rating >= 0 && this.rating <= 10) {
+            return true;
+        }
+        alert('Movie rating must be a number between 0 and 10');
+        return false;
+    }
+    validateTitle() {
+        if(this.title.length > 1) {
+            return true;
+        }
+        alert('Movie title must be at least two characters');
+        return false;
+    }
+}
+
 $('#addMovie').on('click', function(event) {
     event.preventDefault();
     const title = $('#movieTitle').val();
     const rating = $('#movieRating').val();
-    addMovie(title, rating);
+    movies = addMovie(title, rating);     // not sure this is the best way to orchestrate this
+    updateDOM(movies);
 })
 
 $('#movies').on('click', '.remove', function(){
-    $(this).parent().remove();
+    movies.splice(movies.findIndex((movie => movie.id === $(this).parent().attr('id'))), 1);
+    updateDOM(movies);
 })
 
-function validateRating(rating) {
-    if(rating >= 0 && rating <= 10) {
-        return true;
-    }
-    alert('Movie rating must be a number between 0 and 10');
-    return false;
-}
-
-function validateTitle(title) {
-    if(title.length > 1) {
-        return true;
-    }
-    alert('Movie title must be at least two characters');
-    return false;
-}
-
 function addMovie(title, rating) {
-    if(!validateRating(rating)) {
+    const movie = new Movie(title, rating);
+    if(!movie.validateRating()) {
         return;
-    };
-    if(!validateTitle(title)) {
+    }
+    if(!movie.validateTitle()) {
         return;
-    };
-    $('<div>')
-        .text(`${title} rates a ${rating} `)
-        .append($('<button>').text('Remove').addClass('remove'))
-        .appendTo('#movies');
-} 
+    }
+    movie.id = `movie${id}`;
+    id += 1;
+    movies.push(movie);
+    return movies;
+}
+
+function updateDOM(movies) {
+    $('#movies').html('');
+    movies.forEach(movie => {
+        $('<div>')
+            .text(`${movie.title} rates a ${movie.rating} `)
+            .attr('id', movie.id)
+            .append($('<button>').text('Remove').addClass('remove'))
+            .appendTo('#movies');
+    });
+}
